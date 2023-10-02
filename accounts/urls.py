7905bf30066bc538f4fc7151a7bc237d9fe3a42f
Your Name <mdshahreerirfan@gmail.com>
2023-10-03 01:48:15 +0600

    registration form done

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('register/',views.registration,name='register'),
+    path('register/',views.register,name='register'),
     path('profile/',views.profile,name='profile'),
     path('login/',views.singin,name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+
 
 # Create your views here.
 
-def registration(request):
-    return render(request,'accounts/register.html')
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            return redirect('cart')
+    return render(request, 'accounts/register.html', {'form':form})
 
 def profile(request):
     return render(request,'accounts/dashboard.html')
