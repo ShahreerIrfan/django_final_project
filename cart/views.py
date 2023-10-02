3eaea744b360f74ecb1e1a515551dadb0c7072f4
Your Name <mdshahreerirfan@gmail.com>
2023-10-02 18:36:51 +0600

    cart view

--- cart/views.py
+++ cart/views.py
@@ -3,12 +3,7 @@ from store.models import Product
 # Create your views here.
 from .models import Cart, CartItem
 def cart(request):
-    session_id = request.session.session_key
-    cart_id = Cart.objects.filter(cart_id = session_id).exists()
     return render(request, 'cart/cart.html')
-    if cart_id:
-        cart_items = CartItem.objects.filter(cart = session_id)
-        print(cart_items)
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -34,6 +29,9 @@ def add_to_cart(request, product_id):
         cart_id = session_id
         )
         cart.save()
+    
+
+    
     
     return redirect('cart')
     
