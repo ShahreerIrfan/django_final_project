d8f35cba2f5781383c2ca2a9f06a718937402aa5
Your Name <mdshahreerirfan@gmail.com>
2023-10-03 17:49:59 +0600

    Login logout authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm
+from django.contrib.auth import login,logout,authenticate
 
 
 # Create your views here.
@@ -10,12 +11,23 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # login(request, user)
+            login(request, user)
             return redirect('cart')
     return render(request, 'accounts/register.html', {'form':form})
 
 def profile(request):
     return render(request,'accounts/dashboard.html')
 
-def singin(request):
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=user_name,password=password)
+        print(user)
+        login(request,user)
+        return redirect('profile')
     return render(request,'accounts/signin.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
